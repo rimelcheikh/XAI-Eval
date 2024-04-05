e71b6a0ba5467ef695801e808974365476cef3b7
Rim EL CHEIKH <reemelcheikh@hotmail.com>
2024-04-05 14:05:05 +0200

    [WIP] Fixing DAP custom model

--- DAP/custom_DAP_model.py
+++ DAP/custom_DAP_model.py
@@ -14,9 +14,31 @@ from joblib import load
 class DAPModel(Model):
     def __init__(self,model,weights_matrix):
         super(DAPModel, self).__init__()
-        self.model = model
-        self.attributes_layer = CustomSVRActivationLayer()
-        #TODO
+        #self.model = model     
+        self.backbone_layer_0 = model.layers[0]
+        self.backbone_layer_1 = model.layers[1]
+        self.backbone_layer_2 = model.layers[2]
+        self.backbone_layer_3 = model.layers[3]
+        self.backbone_layer_4 = model.layers[4]
+        self.backbone_layer_5 = model.layers[5]
+        self.backbone_layer_6 = model.layers[6]
+        self.backbone_layer_7 = model.layers[7]
+        self.backbone_layer_8 = model.layers[8]
+        self.backbone_layer_9 = model.layers[9]
+        self.backbone_layer_10 = model.layers[10]
+        self.backbone_layer_11 = model.layers[11]
+        self.backbone_layer_12 = model.layers[12]
+        self.backbone_layer_13 = model.layers[13]
+        self.backbone_layer_14 = model.layers[14]
+        self.backbone_layer_15 = model.layers[15]
+        self.backbone_layer_16 = model.layers[16]
+        self.backbone_layer_17 = model.layers[17]
+        self.backbone_layer_18 = model.layers[18]
+        self.backbone_layer_19 = model.layers[19]
+        self.backbone_layer_20 = model.layers[20]
+        self.backbone_layer_21 = model.layers[21]
+        self.GlobalAveragePooling2D =  GlobalAveragePooling2D()
+        self.attributes_layer = CustomSVRActivationLayer() #TODO : define output dim       
         self.class_layer = CustomLabelsLayer(weights_matrix)
         self.weights_matrix = weights_matrix
 
@@ -26,15 +48,66 @@ class DAPModel(Model):
     
 
     
-    def __call__(self,inputs):
+    """def __call__(self,inputs):
         #return inputs#self.model(inputs)
-        x = self.model(inputs)
-        
-        #x = GlobalAveragePooling2D()(x)
-        x = self.attributes_layer(x)
-        return self.class_layer(x)
+        #x = self.model(inputs)
+        x = self.backbone_layer_0(inputs)
+        x = self.backbone_layer_1(x)
+        x = self.backbone_layer_2(x)
+        x = self.backbone_layer_3(x)
+        x = self.backbone_layer_4(x)
+        x = self.backbone_layer_5(x)
+        x = self.backbone_layer_6(x)
+        x = self.backbone_layer_7(x)
+        x = self.backbone_layer_8(x)
+        x = self.backbone_layer_9(x)
+        x = self.backbone_layer_10(x)
+        x = self.backbone_layer_11(x)
+        x = self.backbone_layer_12(x)
+        x = self.backbone_layer_13(x)
+        x = self.backbone_layer_14(x)
+        x = self.backbone_layer_15(x)
+        x = self.backbone_layer_16(x)
+        x = self.backbone_layer_17(x)
+        x = self.backbone_layer_18(x)
+        x = self.backbone_layer_19(x)
+        x = self.backbone_layer_20(x)
+        x = self.backbone_layer_21(x)
+        x = self.GlobalAveragePooling2D(x)  #shape=(64,512)
+        x = self.attributes_layer(x)   #[64, 12]
+        return self.class_layer(x)   #[64, 17]"""
     
-
+    def call(self,inputs):
+        #return inputs#self.model(inputs)
+        #x = self.model(inputs)
+        x = self.backbone_layer_0(inputs)
+        x = self.backbone_layer_1(x)
+        x = self.backbone_layer_2(x)
+        x = self.backbone_layer_3(x)
+        x = self.backbone_layer_4(x)
+        x = self.backbone_layer_5(x)
+        x = self.backbone_layer_6(x)
+        x = self.backbone_layer_7(x)
+        x = self.backbone_layer_8(x)
+        x = self.backbone_layer_9(x)
+        x = self.backbone_layer_10(x)
+        x = self.backbone_layer_11(x)
+        x = self.backbone_layer_12(x)
+        x = self.backbone_layer_13(x)
+        x = self.backbone_layer_14(x)
+        x = self.backbone_layer_15(x)
+        x = self.backbone_layer_16(x)
+        x = self.backbone_layer_17(x)
+        x = self.backbone_layer_18(x)
+        x = self.backbone_layer_19(x)
+        x = self.backbone_layer_20(x)
+        x = self.backbone_layer_21(x)
+        x = self.GlobalAveragePooling2D(x)  #shape=(64,512)
+        x = self.attributes_layer(x)   #[64, 12]
+        return self.class_layer(x)   #[64, 17]
+    
+    """def call(self,inputs):
+        return self(inputs)"""
     
     
     def convert_to_ragged(self,instances):
@@ -81,8 +154,10 @@ class DAPModel(Model):
         # Run backwards pass.
         self.optimizer.minimize(loss, self.trainable_variables[1], tape=tape)
         return self.compute_metrics(x, y, y_pred, sample_weight=None)"""
-        np.shape(self.layers[0].get_weights()[1])
-        gradients = tape.gradient(loss, self.trainable_variables)
+        
+        #np.shape(self.layers[0].get_weights()[1])
+        #self.layers[1].get_weights()[0].shape
+        gradients = tape.gradient(loss, self.trainable_variables[:-2])
 
         # Update weights
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
@@ -141,7 +216,7 @@ class DAPModel(Model):
 
 
         # Compute predictions
-        y_pred = self(x, training=False)
+        y_pred = self(x)#, training=False)
         #y_pred = tf.ragged.boolean_mask(y_pred,masks)
         
         # Updates the metrics tracking the loss
@@ -163,6 +238,7 @@ class CustomLabelsLayer(Layer):
     def __init__(self,weights_matrix,**kwargs):
         super(CustomLabelsLayer, self).__init__(**kwargs)
         self.weights_matrix = weights_matrix
+         #self.output_shape = output_shape #à définir
 
 
     def call(self, inputs):
@@ -177,8 +253,15 @@ class CustomLabelsLayer(Layer):
     def compute_output_shape(self, input_shape):
         return input_shape       
     
-    """def build(self, input_shape):
-        super(CustomLabelsLayer, self).build(input_shape)"""
+    def build(self, input_shape):
+        
+        self.w = self.add_weight(name='kernel',
+                                      shape=(int(input_shape[1]),17, int(input_shape[0])),
+                                      trainable=True)
+        self.b = self.add_weight(shape=(int(input_shape[0]),), initializer="zeros", trainable=False)
+
+        #super(CustomLabelsLayer, self).build(input_shape)
+        super().build(input_shape)
         
         
 
@@ -187,7 +270,7 @@ class CustomSVRActivationLayer(Layer):
     def __init__(self,**kwargs):
         super(CustomSVRActivationLayer, self).__init__(**kwargs)
         self.svr = load('./SV/svr_linear_all.joblib')
-        #self.o_s = o_s
+        
 
     """def build(self, input_shape):
         print('BUILD!!!!!!!!!!',input_shape)
@@ -198,7 +281,7 @@ class CustomSVRActivationLayer(Layer):
         super(CustomSVRActivationLayer, self).build(input_shape)"""
 
     def svr_function(self, inputs):
-        # Convert SymbolicTensor to NumPy array
+        # Convert Tensor to NumPy array __call__
         inputs_np = inputs.numpy()
 
         # Assuming inputs_np is a NumPy array, adjust as needed
@@ -227,6 +310,17 @@ class CustomSVRActivationLayer(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape
+    
+    
+    def build(self, input_shape):
+        #à vérifier la dimension des poids et de input_shape
+        self.w = self.add_weight(name='kernel',
+                                      shape=(int(input_shape[1]),12, int(input_shape[0])),
+                                      trainable=True)
+        self.b = self.add_weight(shape=(int(input_shape[0]),), initializer="zeros", trainable=False)
+
+        #super(CustomSVRActivationLayer, self).build(input_shape)
+        super().build(input_shape)
 
 
 
@@ -242,12 +336,14 @@ def get_DAP_model(targets, concepts, model_name, weights_matrix, mat_pd, X_train
     # add your head on top
     x = base_model.output
     model = Model(base_model.input)
+    
+    
     model = DAPModel(base_model, weights_matrix)
     
     #model = Model(inputs=base_model.input, outputs=x)
 
     model.compile(optimizer=SGD(),
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy (from_logits=False),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
                   metrics=['accuracy'],run_eagerly=True)
     
     from datetime import datetime
@@ -277,7 +373,7 @@ def get_DAP_model(targets, concepts, model_name, weights_matrix, mat_pd, X_train
         # Export the graph
         tf.summary.trace_export(name="my_trace", step=0, profiler_outdir=logdir)"""
     
-    history = model.fit(X_train[:64], y_train[:64], epochs=2, batch_size=16,
+    history = model.fit(X_train[:64], y_train[:64], epochs=1, batch_size=64,
                         validation_data=(X_valid[:32], y_valid[:32]), 
     callbacks=[tensorboard_callback])
     
